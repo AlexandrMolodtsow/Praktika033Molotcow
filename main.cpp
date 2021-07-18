56f2fa1940c5477aa1a4638084469751239bfefa
Alexandr Molodtsow <Sanya04012003@gmail.com>
2021-07-18 18:01:58 +0300

    Исправленный код

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     double b;
     double sum_of_deviations=0;
 
-    //запрос пути к считываемому файлу
+
     if (argc==2)
     {
         Input_file_name=argv[1];
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
     }
 
 
-    //запрос пути к записываемому файлу
+
     if (argc==3)
     {
         Output_file_name=argv[2];
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
         cin>>Output_file_name;
     }
 
-    //чтение из файла, попутно складываем сумму всех чисел и находим количество этих чисел
+
     ifstream in;
     in.open(Input_file_name);
     while (getline(in,line))
@@ -56,15 +56,15 @@ int main(int argc, char *argv[])
     }
     in.close();
 
-    //Вывод дополнительных данных (сумма всех чисел и их количество)
-    cout<<" "<<endl;
-    cout<<"Sum is "<<sum<<endl;
-    cout<<"Quanity is "<<quanity<<endl;
 
-    //Нахождение среднего арифметического
+    cout<<" "<<endl;
+    cout<<"Sum = "<<sum<<endl;
+    cout<<"Quanity = "<<quanity<<endl;
+
+
     arithmetic_mean=sum/quanity;
 
-    //Нахождение дисперсии
+
     for (int i=0; i<array_of_integer.size(); i++)
     {
         b=pow(array_of_integer.at(i)-arithmetic_mean,2);
@@ -72,16 +72,16 @@ int main(int argc, char *argv[])
     }
     dispersion=sum_of_deviations/quanity;
 
-    //вывод результата на экран
-    cout<<" "<<endl;
-    cout<<"Arithmetic mean is "<<arithmetic_mean<<endl;
-    cout<<"Dispersion is "<<dispersion<<endl;
 
-    //запись файла
+    cout<<" "<<endl;
+    cout<<"Arithmetic = "<<arithmetic_mean<<endl;
+    cout<<"Dispersion = "<<dispersion<<endl;
+
+
     ofstream out;
     out.open(Output_file_name);
-    out<<"Arithmetic mean is "<<arithmetic_mean<<endl;
-    out<<"Dispersion is "<<dispersion<<endl;
+    out<<"Arithmetic = "<<arithmetic_mean<<endl;
+    out<<"Dispersion = "<<dispersion<<endl;
     out.close();
     return 0;
 }
